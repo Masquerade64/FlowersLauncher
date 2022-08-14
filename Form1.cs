55b802556f3cc0886104d6d888cb88fc152eec6c
Masquerade <87361986+Masquerade64@users.noreply.github.com>
2022-08-14 11:39:50 +0100

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.IO;
 
 namespace Flowers_Launcher
 {
@@ -42,32 +43,36 @@ namespace Flowers_Launcher
 
         private void PrintempsBtn_Click(object sender, EventArgs e)
         {
+            Directory.SetCurrentDirectory("Printemps");
             Process PlayGame = new Process();
-            PlayGame.StartInfo.FileName = "Printemps\\FLOWERS.EXE";
+            PlayGame.StartInfo.FileName = "FLOWERS.EXE";
             PlayGame.Start();
             Application.Exit();
         }
 
         private void EteBtn_Click(object sender, EventArgs e)
         {
+            Directory.SetCurrentDirectory("Ete");
             Process PlayGame = new Process();
-            PlayGame.StartInfo.FileName = "Ete\\FLOWERS.EXE";
+            PlayGame.StartInfo.FileName = "FLOWERS.EXE";
             PlayGame.Start();
             Application.Exit();
         }
 
         private void AutomneBtn_Click(object sender, EventArgs e)
         {
+            Directory.SetCurrentDirectory("Automne");
             Process PlayGame = new Process();
-            PlayGame.StartInfo.FileName = "Automne\\FLOWERS.EXE";
+            PlayGame.StartInfo.FileName = "FLOWERS.EXE";
             PlayGame.Start();
             Application.Exit();
         }
 
         private void HiverBtn_Click(object sender, EventArgs e)
         {
+            Directory.SetCurrentDirectory("Hiver");
             Process PlayGame = new Process();
-            PlayGame.StartInfo.FileName = "Hiver\\FLOWERS.EXE";
+            PlayGame.StartInfo.FileName = "FLOWERS.EXE";
             PlayGame.Start();
             Application.Exit();
         }
